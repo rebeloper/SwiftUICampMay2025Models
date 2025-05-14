64f4dac8ef4168ee64319c3d3990530bfba07ce5
rebeloper <therebeloper@gmail.com>
2025-05-14 17:22:13 +0300

    score

--- Sources/SwiftUICampMay2025Models/Profile.swift
+++ Sources/SwiftUICampMay2025Models/Profile.swift
@@ -16,24 +16,30 @@ public struct Profile: Identifiable, Codable {
     }
     
     public var name: String
+    public var score: Int
     
-    public init(name: String = "") {
+    public init(name: String = "",
+                score: Int = 0) {
         self.name = name
+        self.score = score
     }
     
     public enum CodingKeys: String, CodingKey {
         case id
-        case name = "name"
+        case name
+        case score
     }
     
     public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self._id = try container.decode(DocumentID<String>.self, forKey: .id)
         self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        self.score = try container.decodeIfPresent(Int.self, forKey: .score) ?? 0
     }
     
     public func encode(to encoder: any Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encodeIfPresent(self.name, forKey: .name)
+        try container.encodeIfPresent(self.score, forKey: .score)
     }
 }
